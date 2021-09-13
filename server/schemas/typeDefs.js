3acd3efdae5b7175e479b87eea44a505e431358b
John Lanni <82123623+JohnLanni619@users.noreply.github.com>
2021-09-13 13:01:46 -0400

    set up apollo server, added typDefs and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,35 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+
+    type Book {
+        bookId: Int
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,42 @@
 const express = require('express');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
+const http = require("http");
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
+// I don't need routes for graphql
+// const routes = require('./routes');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+// create a new Apollo server and pass in our schema data
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers
+// });
 
-app.use(express.urlencoded({ extended: true }));
+let apolloServer = null;
+async function startServer() {
+    apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+}
+startServer();
+const httpserver = http.createServer(app);
+
+// integrate our Apollo server with the Express application as middleware
+// apolloServer.applyMiddleware({ app });
+
+// This was defaulted to true... What does it do?
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -14,8 +44,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// I don't need this for graphql, only rest api
+// app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${apolloServer.graphqlPath}`);
+  });
 });
